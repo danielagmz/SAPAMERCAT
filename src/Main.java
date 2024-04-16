2d132f9cedc03a73d68c3ad6488e48d5275059a4
danielagmz <161260538+danielagmz@users.noreply.github.com>
2024-04-16 13:59:50 +0200

    outline de los menus

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
     public static void main(final String[] args) {
+        // Dar la bienvenida solo cuando se inicia el programa
+        System.out.println("BENVINGUT AL SAPAMERCAT");
+        Menu.menuPrincipal();
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,102 @@
+import megaLibreria.utilities;
+
+import java.util.Scanner;
+
 public class Menu {
+    static Scanner scan = new Scanner(System.in);
+
+    public static void menuPrincipal(){
+        int opcio;
+
+        System.out.println("-----------");
+        System.out.println("-- INICI --");
+        System.out.println("-----------");
+        System.out.println("1) Introduir producte");
+        System.out.println("2) Passar per caixa");
+        System.out.println("3) Mostrar carret de compra");
+        System.out.println("0) acabar");
+
+
+
+        do {
+            opcio= utilities.introducirNumeroEntero(scan, 3, 0, false);
+            //todo
+            switch (opcio){
+                case 1:
+                    submenu();
+                break;
+
+                case 0:
+                break;
+            }
+
+        } while (opcio!=0);
+
+    }
+
+    static void submenu(){
+        int opcio;
+        int input;
+
+
+
+
+        System.out.println("--------------");
+        System.out.println("-- PRODUCTE --");
+        System.out.println("--------------");
+        System.out.println("1) Alimentacio");
+        System.out.println("2) Tèxtil");
+        System.out.println("3) Electronica");
+        System.out.println("0) Tornar");
+
+
+
+        do {
+            opcio= utilities.introducirNumeroEntero(scan, 3, 0, false);
+            switch (opcio){
+                case 1:
+                    System.out.println("Afergir aliment");
+                    introduirAlimentacio();
+                    break;
+                case 0:
+                    menuPrincipal();
+                    break;
+            }
+        } while (opcio!=0);
+
+
+    }
+    static Alimentacio introduirAlimentacio(){
+
+
+
+
+        new Alimentacio()
+
+        return
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
